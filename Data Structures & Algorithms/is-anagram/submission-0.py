d6fc2d05b66a7e512255827faa76b1446bd6afde
Tejasvini Chawla <73694047+TejasviniChawla@users.noreply.github.com>
2026-08-22 01:45:07 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d_s = {}
+        d_t = {}
+        for i in s: 
+            if i in d_s: 
+                d_s[i]+=1
+            else: 
+                d_s[i]= 0
+        for i in t: 
+            if i in d_t: 
+                d_t[i]+=1
+            else: 
+                d_t[i]= 0 
+        
+        return d_s==d_t
